2b01928cd60348ded7bee596b474745f194bb1e7
Martin Karrer <martin@bmalum.com>
2026-05-16 07:53:43 +0200

    Add precompiled NIF checksums for v0.9.0
    Covers 3 of 4 targets (x86_64-apple-darwin still queued for a
    runner). Consumers on these platforms no longer need Rust:
    - aarch64-apple-darwin (Apple Silicon Mac)
    - aarch64-unknown-linux-gnu (ARM Linux / Graviton)
    - x86_64-unknown-linux-gnu (AMD64 Linux)
    
    The x86_64-apple-darwin checksum will be added once the CI job
    completes. Intel Mac users fall back to source build until then.

---
+++ checksum-Elixir.LoroEx.Native.exs
@@ -0,0 +1,5 @@
+%{
+  "https://github.com/bmalum/loro_ex/releases/download/v0.9.0/libloro_nif-v0.9.0-nif-2.17-aarch64-apple-darwin.so.tar.gz" => "sha256:e079d3e61267ca514b8f52ee7ee12dc9c52419fc76176f4bf7d426d5203dbaaf",
+  "https://github.com/bmalum/loro_ex/releases/download/v0.9.0/libloro_nif-v0.9.0-nif-2.17-aarch64-unknown-linux-gnu.so.tar.gz" => "sha256:ba79c74a24ff3dda5ba6a70d3cc94c41fcb030446b90ad9db519d64010980c56",
+  "https://github.com/bmalum/loro_ex/releases/download/v0.9.0/libloro_nif-v0.9.0-nif-2.17-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:12caa215564d74ea1b7fa45108e1394510e88b0e0658841ca8929ab40504703b",
+}
