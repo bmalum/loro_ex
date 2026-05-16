df52637267f0cf446b4db5e62c6ebae4f80e81b2
Martin Karrer <martin@bmalum.com>
2026-05-16 08:04:39 +0200

    Add precompiled NIF checksums for v0.9.1
    3 of 4 targets available:
    - aarch64-apple-darwin (Apple Silicon)
    - aarch64-unknown-linux-gnu (ARM Linux)
    - x86_64-unknown-linux-gnu (AMD64 Linux)
    
    x86_64-apple-darwin (Intel Mac) will be added once the runner
    becomes available. Intel Mac users source-build in the meantime.

---
+++ checksum-Elixir.LoroEx.Native.exs
@@ -0,0 +1,5 @@
+%{
+  "https://github.com/bmalum/loro_ex/releases/download/v0.9.1/libloro_nif-v0.9.1-nif-2.17-aarch64-apple-darwin.so.tar.gz" => "sha256:23f8a38d8ecc820a71147fec080713d2a89c1bea4c2ee9fada03accfa126c752",
+  "https://github.com/bmalum/loro_ex/releases/download/v0.9.1/libloro_nif-v0.9.1-nif-2.17-aarch64-unknown-linux-gnu.so.tar.gz" => "sha256:c179b542b3f362bb56895711bbb15213be91072320a14db33f0a7ca2b8db15b1",
+  "https://github.com/bmalum/loro_ex/releases/download/v0.9.1/libloro_nif-v0.9.1-nif-2.17-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:8166d4f2fd7d657cefd1890113239cf451f0af46961c6fb09e116ef73c4e337d",
+}
