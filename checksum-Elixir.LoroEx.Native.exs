75595ea332be0c8822b7439d03123569bd118af4
Martin Karrer <martin@bmalum.com>
2026-05-16 08:12:40 +0200

    Regenerate checksums with correctly-packaged macOS artifact

---
+++ checksum-Elixir.LoroEx.Native.exs
@@ -0,0 +1,5 @@
+%{
+  "libloro_nif-v0.9.1-nif-2.17-aarch64-apple-darwin.so.tar.gz" => "sha256:cac387f3d7a176227df4c064d64f0a29d9517d186cf488fa5eaeba981edf8b96",
+  "libloro_nif-v0.9.1-nif-2.17-aarch64-unknown-linux-gnu.so.tar.gz" => "sha256:da16292a2d3142fd78632e65a3294f29dcf8b54683f5591222ab7c8e9bb29034",
+  "libloro_nif-v0.9.1-nif-2.17-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:77c1f84b4b4ae9260c941ac87a3f0e6dabda6678e78c691528b54418fea0f80b",
+}
